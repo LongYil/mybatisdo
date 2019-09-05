332ef0fe0b5006e41a9ea0eadd9cc895cdfd3fc1
li-yin-long <18312884980@163.com>
2019-09-05 18:34:02 +0800

    Initial commit

---
+++ src/main/java/com/longdatech/mybatisdo/MybatisdoApplication.java
@@ -0,0 +1,13 @@
+package com.longdatech.mybatisdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisdoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisdoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longdatech/mybatisdo/DemoTest.java
@@ -0,0 +1,20 @@
+package com.longdatech.mybatisdo;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+public class DemoTest {
+
+    @Test
+    public void test1() throws Exception{
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+    }
+}
