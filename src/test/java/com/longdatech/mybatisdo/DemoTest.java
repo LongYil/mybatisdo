cdfabdf21727f0982d787c2b39ca8f6fd49cd5f4
li-yin-long <18312884980@163.com>
2019-09-05 18:39:25 +0800

    首次提交。

--- src/test/java/com/longdatech/mybatisdo/DemoTest.java
+++ src/test/java/com/longdatech/mybatisdo/DemoTest.java
@@ -1,6 +1,7 @@
 package com.longdatech.mybatisdo;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
@@ -14,6 +15,7 @@ public class DemoTest {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
 
 
     }
