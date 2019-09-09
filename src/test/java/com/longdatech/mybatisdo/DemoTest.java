2435c854824e34f06bfebcd778df6296158fda1a
li-yin-long <18312884980@163.com>
2019-09-09 21:23:50 +0800

    首次合并

--- src/test/java/com/longdatech/mybatisdo/DemoTest.java
+++ src/test/java/com/longdatech/mybatisdo/DemoTest.java
@@ -6,6 +6,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DemoTest {
 
@@ -14,7 +17,17 @@ public class DemoTest {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
 
-
+    @Test
+    public void test(){
+        Integer[] a = new Integer[2];
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        Integer[] b = list.toArray(a);
+        System.out.println(a);
+        System.out.println(b);
     }
 }
